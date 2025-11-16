c663159d70373c29a461b4e4a7ab656405a24657
riti <abidariti690@gmail.com>
2025-11-16 19:36:02 +0600

    added Scene1.fxml file

--- src/main/java/com/example/javaassignment2207063/HelloApplication.java
+++ src/main/java/com/example/javaassignment2207063/HelloApplication.java
@@ -11,6 +11,7 @@ import java.util.Objects;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        SceneLoader.SetStage(stage);
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
--- src/main/java/com/example/javaassignment2207063/HelloController.java
+++ src/main/java/com/example/javaassignment2207063/HelloController.java
@@ -4,11 +4,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void calculateGPA(){
+        try {
+            SceneLoader.switchScene("Scene1.fxml");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
+
 }
