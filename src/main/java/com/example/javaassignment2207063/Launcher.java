d81745c6f4271fdf6a234b800bd53b25e8b89a0d
riti <abidariti690@gmail.com>
2025-11-16 16:55:21 +0600

    Project created

---
+++ src/main/java/com/example/javaassignment2207063/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.javaassignment2207063;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/module-info.java
@@ -0,0 +1,8 @@
+module com.example.javaassignment2207063 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.javaassignment2207063 to javafx.fxml;
+    exports com.example.javaassignment2207063;
+}
